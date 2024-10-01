0a912bea0828b7ad53b98ca5a07f81a6be93b948
Sabin Jose <sabin.jose@ricoh-europe.com>
2024-10-01 16:23:45 +0100

    Added python scripts

---
+++ test_main.py
@@ -0,0 +1,14 @@
+import unittest
+from main import add
+
+
+class TestAddFunction(unittest.TestCase):
+    def test_add_positive(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_add_negative(self):
+        self.assertEqual(add(-1, -1), -2)
+
+
+if __name__ == "__main__":
+    unittest.main()
